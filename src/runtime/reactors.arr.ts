8cebc8c76f4dcb58ba2bb86b671ba9717fc075de
Alexander Vo <randomcivvy0121@gmail.com>
2019-09-19 14:21:37 -0700

    Begin writing makeReactor() and makeReactorRaw()

--- src/runtime/reactors.arr.ts
+++ src/runtime/reactors.arr.ts
@@ -11,6 +11,32 @@ const isEvent = reactorEvents["is-Event"];
 
 var externalInteractionHandler = null;
 
+// TODO(alex): parameterize Reactor over init?
+export interface Reactor {
+  init: any,
+  "get-value": () => any,
+}
+
 function setInteract(newInteract) {
   externalInteractionHandler = newInteract;
+}
+
+function makeReactor(init: any, fields: object): Reactor {
+  let handlerDict = {};
+  Object.keys(fields).forEach((f) => {
+    handlerDict[f] = fields[f];
+  });
+
+  return makeReactorRaw(init, handlerDict, false, []);
+}
+
+function makeReactorRaw(init: any, handlers: object, 
+                        tracing: boolean, trace: any[]): Reactor {
+  return {
+    init: init,
+
+    "get-value": function() {
+      return init;
+    }
+  };
 }
